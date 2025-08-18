c91c960e96cb455b4a9dba6383f7f4bca27b2fff
Priyanshu Vats <priyanshuvatsbal@gmail.com>
2025-08-18 23:44:17 +0530

    added firebase config

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,29 +1,10 @@
 
- // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 const Footer = () => {
   
  
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB6F4U7XiN_9PCCXs_BaBbjLUMfhqYjNjo",
-  authDomain: "threeq-889e2.firebaseapp.com",
-  projectId: "threeq-889e2",
-  storageBucket: "threeq-889e2.firebasestorage.app",
-  messagingSenderId: "902072385204",
-  appId: "1:902072385204:web:f30836dba7bd3519bd61e1",
-  measurementId: "G-CPTJXHJPSR"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
   return (
